93ec16d6b7a77a9efb9b66c8be419861d2de1e8d
Stefan Kjartansson <esteban.supreme@gmail.com>
2011-10-25 13:33:13 +0000

    Renamed nginx to context and generate_nginx to server_conf.

---
+++ djserverconf/conf/context.py
@@ -0,0 +1,100 @@
+import os
+import sys
+
+from django.conf import settings
+
+
+def get_server_context(**kwargs):
+
+    context = {
+        'name': getattr(settings, 'SERVER_APP_NAME',
+            settings.ROOT_URLCONF.split('.')[0]),
+        'domain': getattr(settings, 'SERVER_DOMAIN',
+            'test.com'),
+        'upstream': getattr(settings, 'SERVER_UPSTREAM',
+            '%s:%d' % (
+                 getattr(settings,
+                    'SERVER_UPSTREAM_HOST', '127.0.0.1'),
+                 getattr(settings,
+                    'SERVER_UPSTREAM_PORT', (8000 if
+                        ('gunicorn' in settings.INSTALLED_APPS) else 3031)),
+            )),
+        'ssl': getattr(settings, 'SERVER_USE_SSL', False),
+        'ssl_rewrite': getattr(settings, 'SERVER_SSL_REWRITE', False),
+        'static_root': settings.STATIC_ROOT.rstrip(' /'),
+        'static_url': getattr(settings,
+            'STATIC_URL', '/static/').rstrip(' /'),
+        'media_root': settings.MEDIA_ROOT.rstrip(' /'),
+        'media_url': settings.MEDIA_URL.rstrip(' /'),
+        'using_virtual_env': hasattr(sys, 'real_prefix'),
+        'python_executable': sys.executable,
+        'using_celery': ('djcelery' in settings.INSTALLED_APPS),
+        'disable_celerybeat': getattr(settings,
+            'SERVER_DISABLE_CELERYBEAT', False),
+    }
+
+    context.update({
+        'upstream_std_log_path': getattr(settings,
+            'SERVER_UPSTREAM_LOGPATH',
+            '/var/log/%s' % context['name']),
+    })
+
+    ex_head, ex_tail = os.path.split(sys.executable)
+
+    #Detect uwsgi
+    if os.path.isfile(os.path.join(ex_head, 'uwsgi')):
+        context.update({
+            'reverse': 'uwsgi',
+            'uwsgi_path': os.path.join(ex_head, 'uwsgi')})
+
+    #Detect gunicorn
+    if 'gunicorn' in settings.INSTALLED_APPS:
+        context.update({
+            'reverse': 'gunicorn'})
+
+    #get project root
+    root = getattr(settings, 'APP_ROOT',
+        getattr(settings, 'SITE_ROOT', None))
+    if not root:
+        root = os.getcwd()
+
+    context.update({'home': root,
+        'wsgi_script_alias': getattr(settings,
+            'WSGI_SCRIPT_ALIAS',
+            '%s/django.wsgi' % os.path.split(root)[0]),
+    })
+
+    if context['ssl']:
+        crt = getattr(settings, 'SERVER_SSL_CRT_PATH', None)
+        key = getattr(settings, 'SERVER_SSL_KEY_PATH', None)
+        ca_path = getattr(settings, 'SERVER_SSL_CA_PATH', None)
+        ca_file = getattr(settings, 'SERVER_SSL_CA_FILE', None)
+
+        #bit iffy, relies on there being a ssl folder on the same
+        #level as the django project.
+
+        if not (crt and key):
+            head, tail =  os.path.split(root)
+            crt = '%s/ssl/%s.crt' % (head, context['name'])
+            key = '%s/ssl/%s.key' % (head, context['name'])
+            ca_path = '%s/ssl/CA' % (head)
+            ca_file = '%s/ssl/CA/%s.crt' % (head, context['name'])
+
+        context.update({
+            'ssl_crt': crt,
+            'ssl_key': key,
+            'ssl_ca_path': ca_path,
+            'ssl_ca_file': ca_file,
+        })
+
+    if context['using_virtual_env']:
+        venv_path, venv_tail =  os.path.split(ex_head)
+        context.update({'virtual_env': venv_path})
+
+    if hasattr(settings, 'SERVER_SUPERVISOR_USER'):
+        context.update({'supervisor_user': settings.SERVER_SUPERVISOR_USER})
+        #defaults to www-data in the templates
+
+    context.update(kwargs)
+
+    return context
---
+++ djserverconf/management/commands/server_conf.py
@@ -0,0 +1,91 @@
+from textwrap import dedent
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
+
+from djserverconf.conf.context import get_server_context
+
+
+class Command(BaseCommand):
+
+    args = "[<command> [<process>, ...]]"
+
+    help = dedent("""
+
+        Generates server configuration files.
+
+        TODO: docs
+           """).strip()
+
+    option_list = BaseCommand.option_list + (
+        make_option('--supervisor',
+                action='store_true',
+                dest='supervisor',
+                default=False,
+                help='Generate supervisor configuration file'),
+        make_option('--nginx',
+                action='store_true',
+                dest='nginx',
+                default=False,
+                help='Generate nginx configuration file'),
+        make_option('--apache',
+                action='store_true',
+                dest='apache',
+                default=False,
+                help='Generate apache configuration file'),
+    )
+
+
+    def handle_supervisor(self, context, f=None):
+
+        stream = f or self.stdout
+
+        if context['reverse'] == 'uwsgi':
+            stream.write('#Auto generated uwsgi supervisor configuration:\n')
+            stream.write(render_to_string('djserverconf/uwsgi_supervisor.conf', context) + '\n')
+
+        elif context['reverse'] == 'gunicorn':
+            stream.write('#Auto generated gunicorn supervisor configuration:\n')
+            stream.write(render_to_string('djserverconf/gunicorn_supervisor.conf', context) + '\n')
+
+        if context['using_celery']:
+            stream.write('#Auto generated celery supervisor configuration:\n')
+            stream.write(render_to_string('djserverconf/celery_supervisor.conf', context) + '\n')
+
+            if not context.get('disable_celerybeat', False):
+                stream.write('#Auto generated celery_beat supervisor configuration:\n')
+                stream.write(render_to_string('djserverconf/celerybeat_supervisor.conf', context) + '\n')
+
+
+    def handle(self, *args, **options):
+
+        context = get_server_context()
+        if not any(map(options.get, ['nginx', 'apache', 'supervisor'])):
+            import pprint
+            pp = pprint.PrettyPrinter(indent=4, stream=self.stdout)
+            self.stdout.write('Auto-detected server context:\n')
+            pp.pprint(context)
+
+            self.stdout.write(('-' * 80) + '\n')
+            self.stdout.write('Auto generated nginx configuration example:\n')
+            self.stdout.write(render_to_string('djserverconf/nginx.conf', context) + '\n')
+            self.stdout.write(('-' * 80) + '\n')
+
+            self.stdout.write('Auto generated apache configuration example:\n')
+            self.stdout.write(render_to_string('djserverconf/apache.conf', context) + '\n')
+            self.stdout.write(('-' * 80) + '\n')
+
+            self.handle_supervisor(context)
+
+        if options['nginx']:
+            with open('%s_nginx.conf' % context['name'], 'w') as f:
+                f.write(render_to_string('djserverconf/nginx.conf', context))
+
+        if options['supervisor']:
+            with open('%s_sv.conf' % context['name'], 'w') as f:
+                self.handle_supervisor(context, f)
+
+        if options['apache']:
+            with open('%s_apache' % context['name'], 'w') as f:
+                f.write(render_to_string('djserverconf/apache.conf', context))
